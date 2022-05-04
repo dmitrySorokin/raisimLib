0b41b368d4728d157f67c85cf9078b01c1a66bbb
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2022-05-04 15:47:54 +0300

    up

--- raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/runner.py
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/runner.py
@@ -13,119 +13,118 @@ import torch
 import argparse
 
 
-# task specification
-task_name = "anymal_locomotion"
+if __name__ == '__main__':
+    # task specification
+    task_name = "anymal_locomotion"
 
-# configuration
-parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--mode', help='set mode either train or test', type=str, default='train')
-parser.add_argument('-w', '--weight', help='pre-trained weight path', type=str, default='')
-args = parser.parse_args()
-mode = args.mode
-weight_path = args.weight
+    # configuration
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--weight', help='pre-trained weight path', type=str, default=None)
+    args = parser.parse_args()
+    weight_path = args.weight
 
-# check if gpu is available
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # check if gpu is available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# directories
-task_path = os.path.dirname(os.path.realpath(__file__))
-home_path = task_path + "/../../../../.."
+    # directories
+    task_path = os.path.dirname(os.path.realpath(__file__))
+    home_path = task_path + "/../../../../.."
 
-# config
-cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
+    # config
+    cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
 
-# create environment from the configuration file
-env = VecEnv(RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)))
-
-# shortcuts
-ob_dim = env.num_obs
-act_dim = env.num_acts
-num_threads = cfg['environment']['num_threads']
-
-# Training
-n_steps = 128
-total_steps = n_steps * env.num_envs
-avg_rewards = np.zeros(env.num_envs)
-avg_steps = np.zeros(env.num_envs)
-
-actor = ppo_module.Actor(
-    ppo_module.MLP(cfg['architecture']['policy_net'], nn.LeakyReLU, ob_dim, act_dim),
-    ppo_module.MultivariateGaussianDiagonalCovariance(
-        act_dim, env.num_envs, 1.0, NormalSampler(act_dim), cfg['seed']),
-    device
-)
-
-critic = ppo_module.Critic(
-    ppo_module.MLP(cfg['architecture']['value_net'], nn.LeakyReLU, ob_dim, 1),
-    device
-)
-
-saver = ConfigurationSaver(log_dir=home_path + "/raisimGymTorch/data/"+task_name,
-                           save_items=[task_path + "/cfg.yaml", task_path + "/Environment.hpp"])
-
-# tensorboard_launcher(saver.data_dir+"/..")  # press refresh (F5) after the first ppo update
-
-ppo = PPO.PPO(
-    actor=actor,
-    critic=critic,
-    num_envs=cfg['environment']['num_envs'],
-    num_transitions_per_env=n_steps,
-    num_learning_epochs=4,
-    gamma=0.996,
-    lam=0.95,
-    num_mini_batches=4,
-    device=device,
-    log_dir=saver.data_dir,
-    shuffle_batch=True,
-)
-
-if mode == 'retrain':
-    load_param(weight_path, env, actor, critic, ppo.optimizer, saver.data_dir)
-
-for update in range(1000000):
-    start = time.time()
+    # create environment from the configuration file
+    env = VecEnv(RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)))
     env.reset()
 
-    if update % cfg['environment']['eval_every_n'] == 0:
-        print("Visualizing and evaluating the current policy")
-        torch.save({
-            'actor_architecture_state_dict': actor.architecture.state_dict(),
-            'actor_distribution_state_dict': actor.distribution.state_dict(),
-            'critic_architecture_state_dict': critic.architecture.state_dict(),
-            'optimizer_state_dict': ppo.optimizer.state_dict(),
-        }, saver.data_dir+"/full_"+str(update)+'.pt')
-        # we create another graph just to demonstrate the save/load method
-        env.save_scaling(saver.data_dir, str(update))
+    # shortcuts
+    ob_dim = env.num_obs
+    act_dim = env.num_acts
+    num_threads = cfg['environment']['num_threads']
 
-    # actual training
-    for step in range(n_steps):
+    # Training
+    n_steps = 128
+    total_steps = n_steps * env.num_envs
+    avg_rewards = np.zeros(env.num_envs)
+    avg_steps = np.zeros(env.num_envs)
+
+    actor = ppo_module.Actor(
+        ppo_module.MLP(cfg['architecture']['policy_net'], nn.LeakyReLU, ob_dim, act_dim),
+        ppo_module.MultivariateGaussianDiagonalCovariance(
+            act_dim, env.num_envs, 1.0, NormalSampler(act_dim), cfg['seed']),
+        device
+    )
+
+    critic = ppo_module.Critic(
+        ppo_module.MLP(cfg['architecture']['value_net'], nn.LeakyReLU, ob_dim, 1),
+        device
+    )
+
+    saver = ConfigurationSaver(log_dir=home_path + "/raisimGymTorch/data/"+task_name,
+                               save_items=[task_path + "/cfg.yaml", task_path + "/Environment.hpp"])
+
+    # tensorboard_launcher(saver.data_dir+"/..")  # press refresh (F5) after the first ppo update
+
+    ppo = PPO.PPO(
+        actor=actor,
+        critic=critic,
+        num_envs=cfg['environment']['num_envs'],
+        num_transitions_per_env=n_steps,
+        num_learning_epochs=4,
+        gamma=0.996,
+        lam=0.95,
+        num_mini_batches=4,
+        device=device,
+        log_dir=saver.data_dir,
+        shuffle_batch=True,
+    )
+
+    if weight_path is not None:
+        load_param(weight_path, env, actor, critic, ppo.optimizer, saver.data_dir)
+
+    for update in range(1000000):
+        start = time.time()
+
+        if update % cfg['environment']['eval_every_n'] == 0:
+            print("Visualizing and evaluating the current policy")
+            torch.save({
+                'actor_architecture_state_dict': actor.architecture.state_dict(),
+                'actor_distribution_state_dict': actor.distribution.state_dict(),
+                'critic_architecture_state_dict': critic.architecture.state_dict(),
+                'optimizer_state_dict': ppo.optimizer.state_dict(),
+            }, saver.data_dir+"/full_"+str(update)+'.pt')
+            # we create another graph just to demonstrate the save/load method
+            env.save_scaling(saver.data_dir, str(update))
+
+        # actual training
+        for step in range(n_steps):
+            obs = env.observe()
+            action = ppo.act(obs)
+            reward, dones = env.step(action)
+            ppo.step(value_obs=obs, rews=reward, dones=dones)
+            avg_rewards = (avg_rewards + reward) * (1 - dones)
+            avg_steps = (avg_steps + 1) * (1 - dones)
+
+        # take st step to get value obs
         obs = env.observe()
-        action = ppo.act(obs)
-        reward, dones = env.step(action)
-        ppo.step(value_obs=obs, rews=reward, dones=dones)
-        avg_rewards = avg_rewards * (1 - dones) + reward
-        avg_steps = avg_steps * (1 - dones) + 1
+        ppo.update(actor_obs=obs, value_obs=obs, log_this_iteration=update % 10 == 0, update=update)
+        average_ll_performance = np.mean(avg_rewards)
+        average_steps = np.mean(avg_steps)
 
-    # take st step to get value obs
-    obs = env.observe()
-    ppo.update(actor_obs=obs, value_obs=obs, log_this_iteration=update % 10 == 0, update=update)
-    average_ll_performance = np.mean(avg_rewards)
-    average_steps = np.mean(avg_steps)
+        actor.update()
+        # actor.distribution.enforce_minimum_std((torch.ones(12)*0.2).to(device))
 
-    actor.update()
-    # actor.distribution.enforce_minimum_std((torch.ones(12)*0.2).to(device))
+        # curriculum update. Implement it in Environment.hpp
+        env.curriculum_callback()
 
-    # curriculum update. Implement it in Environment.hpp
-    env.curriculum_callback()
+        end = time.time()
 
-    end = time.time()
-
-    print('----------------------------------------------------')
-    print('{:>6}th iteration'.format(update))
-    print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(average_ll_performance)))
-    print('{:<40} {:>6}'.format("steps: ", '{:0.6f}'.format(average_steps)))
-    print('{:<40} {:>6}'.format("time elapsed in this iteration: ", '{:6.4f}'.format(end - start)))
-    print('{:<40} {:>6}'.format("fps: ", '{:6.0f}'.format(total_steps / (end - start))))
-    print('{:<40} {:>6}'.format("real time factor: ", '{:6.0f}'.format(total_steps / (end - start)
-                                                                       * cfg['environment']['control_dt'])))
-    print('----------------------------------------------------\n')
+        print('----------------------------------------------------')
+        print('{:>6}th iteration'.format(update))
+        print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(average_ll_performance)))
+        print('{:<40} {:>6}'.format("steps: ", '{:0.6f}'.format(average_steps)))
+        print('{:<40} {:>6}'.format("time elapsed in this iteration: ", '{:6.4f}'.format(end - start)))
+        print('{:<40} {:>6}'.format("fps: ", '{:6.0f}'.format(total_steps / (end - start))))
+        print('{:<40} {:>6}'.format("real time factor: ", '{:6.0f}'.format(total_steps / (end - start)
+                                                                           * cfg['environment']['control_dt'])))
+        print('----------------------------------------------------\n')
--- raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/tester.py
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/tester.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from ruamel.yaml import YAML, dump, RoundTripDumper
 from raisimGymTorch.env.bin import rsg_a1
 from raisimGymTorch.env.RaisimGymVecEnv import RaisimGymVecEnv as VecEnv
@@ -7,73 +6,82 @@ import os
 import time
 import torch
 import argparse
+import numpy as np
+from tqdm import trange
 
-# configuration
-parser = argparse.ArgumentParser()
-parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
-parser.add_argument('--viz', action='store_true')
-args = parser.parse_args()
 
-# check if gpu is available
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if __name__ == '__main__':
+    # configuration
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
+    parser.add_argument('--viz', action='store_true')
+    args = parser.parse_args()
 
-# directories
-task_path = os.path.dirname(os.path.realpath(__file__))
-log_path = "/".join(args.weight.split("/")[:-1])
-home_path = task_path + "/../../../../.."
+    # check if gpu is available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# config
-cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
+    # directories
+    task_path = os.path.dirname(os.path.realpath(__file__))
+    log_path = "/".join(args.weight.split("/")[:-1])
+    home_path = task_path + "/../../../../.."
 
-# create environment from the configuration file
-cfg['environment']['num_envs'] = 1
-cfg['environment']['k_0'] = 1.0
+    # config
+    cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
 
-env = VecEnv(
-    rsg_a1.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)),
-    normalize_ob=True
-)
-env.reset()
+    # create environment from the configuration file
+    cfg['environment']['num_envs'] = 1
+    cfg['environment']['k_0'] = 1.0
 
-# shortcuts
-ob_dim = env.num_obs
-act_dim = env.num_acts
+    env = VecEnv(
+        rsg_a1.RaisimGymEnv(home_path + "/rsc", dump(cfg['environment'], Dumper=RoundTripDumper)),
+        normalize_ob=True
+    )
+    env.reset()
 
-weight_path = args.weight
-iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
-weight_dir = weight_path.rsplit('/', 1)[0] + '/'
+    # shortcuts
+    ob_dim = env.num_obs
+    act_dim = env.num_acts
 
-if weight_path == "":
-    print("Can't find trained weight, please provide a trained weight with --weight switch\n")
-else:
-    print("Loaded weight from {}\n".format(weight_path))
+    weight_path = args.weight
+    iteration_number = weight_path.rsplit('/', 1)[1].split('_', 1)[1].rsplit('.', 1)[0]
+    weight_dir = weight_path.rsplit('/', 1)[0] + '/'
 
-    print("Visualizing and evaluating the policy: ", weight_path)
-    loaded_graph = ppo_module.MLP(cfg['architecture']['policy_net'], torch.nn.LeakyReLU, ob_dim, act_dim)
-    loaded_graph.load_state_dict(torch.load(weight_path, map_location=device)['actor_architecture_state_dict'])
+    if weight_path == "":
+        print("Can't find trained weight, please provide a trained weight with --weight switch\n")
+    else:
+        print("Loaded weight from {}\n".format(weight_path))
 
-    env.load_scaling(weight_dir, int(iteration_number))
-    if args.viz:
-        env.turn_on_visualization()
+        print("Visualizing and evaluating the policy: ", weight_path)
+        loaded_graph = ppo_module.MLP(cfg['architecture']['policy_net'], torch.nn.LeakyReLU, ob_dim, act_dim)
+        loaded_graph.load_state_dict(torch.load(weight_path, map_location=device)['actor_architecture_state_dict'])
 
-    for episode in range(100):
-        done = False
-        reward_ll_sum = 0
-        steps = 0
-        while not done:
-            steps += 1
-            if args.viz:
-                time.sleep(0.01)
-            obs = env.observe(False)
-            action_ll = loaded_graph.architecture(torch.from_numpy(obs).cpu())
-            reward_ll, dones = env.step(action_ll.cpu().detach().numpy())
-            reward_ll_sum += reward_ll[0]
-            done = dones[0]
-        print('----------------------------------------------------')
-        print(steps)
-        print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(reward_ll_sum)))
-        print('{:<40} {:>6}'.format("time elapsed [sec]: ", '{:6.4f}'.format(steps * 0.01)))
-        print('----------------------------------------------------\n')
+        env.load_scaling(weight_dir, int(iteration_number))
+        if args.viz:
+            env.turn_on_visualization()
 
-    if args.viz:
-        env.turn_off_visualization()
+        episode_rewards = []
+        episode_steps = []
+        for episode in trange(10):
+            done = False
+            total_reward = 0
+            steps = 0
+            while not done:
+                steps += 1
+                if args.viz:
+                    time.sleep(0.01)
+                obs = env.observe(False)
+                action = loaded_graph.architecture(torch.from_numpy(obs).cpu())
+                rewards, dones = env.step(action.cpu().detach().numpy())
+                total_reward += rewards[0]
+                done = dones[0]
+            episode_rewards.append(total_reward)
+            episode_steps.append(steps)
+            print('----------------------------------------------------')
+            print('{:<40} {:>6}'.format("average ll reward: ", '{:0.10f}'.format(total_reward)))
+            print('{:<40} {:>6}'.format("time elapsed [sec]: ", '{:6.4f}'.format(steps * 0.01)))
+            print('----------------------------------------------------\n')
+        print(f'return {np.mean(episode_rewards)} +- {np.std(episode_rewards)}')
+        print(f'steps {np.mean(episode_steps)} +- {np.std(episode_steps)}')
+
+        if args.viz:
+            env.turn_off_visualization()
