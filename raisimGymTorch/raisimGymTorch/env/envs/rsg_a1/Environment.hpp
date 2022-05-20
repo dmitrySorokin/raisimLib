fc0453a2325e3a133679917987aa79737a71aa00
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2022-05-20 14:41:27 +0300

    123

--- raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/Environment.hpp
+++ raisimGymTorch/raisimGymTorch/env/envs/rsg_a1/Environment.hpp
@@ -323,7 +323,7 @@ public:
         // Terminal condition
         double euler_angles[3];
         raisim::quatToEulerVec(&gc_[3], euler_angles);
-        if (gc_[2] < 0.28 || fabs(euler_angles[1]) > 0.2) {
+        if (gc_[2] < 0.28) {
             terminalReward = float(terminalRewardCoeff_);
             return true;
         }
